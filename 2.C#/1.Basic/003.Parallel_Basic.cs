621ea4047c97eb29e39d73f9268b1ac2019230b6
nongsang <shdtkd37@gmail.com>
2019-06-04 01:18:57 +0900

    6월 4일 병렬 기초

---
+++ 2.C#/1.Basic/003.Parallel_Basic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Threading;         // 스레드 관련 기능을 쓰려면 추가
+
+namespace CPPPP
+{
+    class Parallel_Basic
+    {
+        public static void Main()
+        {
+            WriteLine("피보나치 수열 구하기 (종료 'x')");
+
+            while(true)
+            {
+                Write("숫자를 입력하세요. : ");
+                string input = ReadLine();      // 문자열 입력
+
+                if(input.Equals("x", StringComparison.OrdinalIgnoreCase))   // x가 입력되면 프로그램 종료
+                {
+                    WriteLine("프로그램 종료");
+                    break;
+                }
+
+                //WriteLine("{0}", Fib(int.Parse(input)));    // 기본적으로 메서드는 동기방식으로 동작한다.
+                                                              // 오래 걸리면 x를 입력해서 탈출해야 하지만 메서드가 실행되는 동안 다른 작업이 불가능하다.
+                                                              // 동기방식의 한계
+                Thread t = new Thread(() => WriteLine(Fib(int.Parse(input))));  // 서브 스레드를 생성하여 연산을 맡긴다.
+                                                                                // 메인 스레드는 서브 스레드를 생성하고 x입력만 들어오기만을 기다린다.
+                                                                                // while문 안에 스레드 생성이 있다고 계속 생성하는 것이 아니다.
+                                                                                // 여전히 동기방식이지만 스레드를 생성해서 전담하는 방식으로 동기방식의 문제를 해결했다.
+                                                                                // A 스레드, B 스레드 나름대로 동시에 일을 시키는 것이기 때문에 병렬(Parallel)처리를 한 것이다.
+                t.Start();
+            }
+        }
+
+        static int Fib(int n)       // 피보나치 수열, 입력값이 커지면 기하급수적으로 성능이 하락한다.
+        {
+            if (0 == n) return 0;
+            else if (1 == n) return 1;
+            else return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
