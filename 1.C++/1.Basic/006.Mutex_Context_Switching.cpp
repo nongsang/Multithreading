70ae6fdbb988a5d86902cb145eb796f82ed42d06
nongsang <shdtkd37@gmail.com>
2019-06-06 03:19:25 +0900

    6월 6일 상호배재와 문맥교환

--- 1.C++/1.Basic/006.Mutex_Context_Switching.cpp
+++ 1.C++/1.Basic/006.Mutex_Context_Switching.cpp
@@ -12,7 +12,7 @@ int sum;
 
 void Thread_Func(int num_threads)
 {
-	for( auto i = 0; i < 50000000 / num_threads; ++i )	// 스레드의 갯수만큼 전체 할 일을 나누어서 실행한다.
+	for( auto i = 0; i < 50000000 / num_threads; ++i )
 	{
 		mylock.lock();		// 한번에 한개의 스레드만 접근할 수 있게 잠금
 							// 하나의 스레드만 잠금을 얻고 다음 작업을 할 수 있다.
@@ -29,26 +29,26 @@ void Thread_Func(int num_threads)
 
 int main()
 {
-	vector<thread *> worker_threads;	// 스레드풀
+	vector<thread *> worker_threads;
 
-	for( auto i = 1; i <= 32; i += i )	// 스레드가 1개 ~ 32개일 때 성능을 측정
+	for( auto i = 1; i <= 32; i += i )
 	{
 		sum = 0;
 		worker_threads.clear();
 
-		auto start = high_resolution_clock::now();	// 시간 측정 시작
+		auto start = high_resolution_clock::now();
 
-		for( auto j = 1; j <= i; ++j )								// 스레드 갯수만큼
-			worker_threads.push_back(new thread{ Thread_Func, i });	// 스레드를 생성하여 스레드풀에 저장
+		for( auto j = 1; j <= i; ++j )
+			worker_threads.push_back(new thread{ Thread_Func, i });
 
-		for( auto t : worker_threads )	// 스레드풀에 있는 모든 스레드
-			t->join();					// 대기
+		for( auto t : worker_threads )
+			t->join();
 
-		auto du = high_resolution_clock::now() - start;	// 시간 측정 끝
+		auto du = high_resolution_clock::now() - start;
 
-		cout << "스레드 갯수 : " << i << " / sum : " << sum << " / ";	// 결과 출력
+		cout << "스레드 갯수 : " << i << " / sum : " << sum << " / ";
 
-		cout << static_cast<double>(duration_cast<milliseconds>(du).count()) / 1000 << " msecs\n";	// 시간 출력
+		cout << static_cast<double>(duration_cast<milliseconds>(du).count()) / 1000 << " msecs\n";
 	}
 }
 
