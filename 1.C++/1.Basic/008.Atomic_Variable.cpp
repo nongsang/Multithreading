7de6f8a99e7e1a77d85b37bdb5ebbcd165b16b79
nongsang <shdtkd37@gmail.com>
2019-06-06 18:29:37 +0900

    6월 6일 아토믹 변수 생성

--- 1.C++/1.Basic/008.Atomic_Variable.cpp
+++ 1.C++/1.Basic/008.Atomic_Variable.cpp
@@ -37,9 +37,9 @@ int main()
 
 		auto du = high_resolution_clock::now() - start;
 
-		cout << "½º·¹µå °¹¼ö : " << i << " / sum : " << sum << " / ";
+		cout << "threads : " << i << " / sum : " << sum << " / ";
 
-		cout << static_cast<double>(duration_cast<microseconds>(du).count()) / 1000000 << " ¥ìs\n";
+		cout << "time : " << static_cast<double>(duration_cast<microseconds>(du).count()) / 1000000 << " ¥ìs\n";
 	}
 }
 
